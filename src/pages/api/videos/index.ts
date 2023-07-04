768b18853de3a0ad4ea67c209f3f286785aebee5
Junior Prado <juniorcvnnn@gmail.com>
2023-07-04 15:24:57 -0300

    adding json file with URL information

--- src/pages/api/videos/index.ts
+++ src/pages/api/videos/index.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-type Data = {
-  name: string
+import {videos} from '@/data/data.json';
+type TVideos = {
+  title: string,
+  url:string,
+  date:string
 }
 
 export default function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<Data>
+	res: NextApiResponse<TVideos[]>
 ) {
-	res.status(200).json({ name: 'John Doe' });
+	res.status(200).json(videos);
 }
