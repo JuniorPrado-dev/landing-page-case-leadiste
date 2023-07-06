dcb43a37040b91425713c0347b4f6df2254d024c
Junior Prado <juniorcvnnn@gmail.com>
2023-07-06 19:54:49 -0300

    add sorting mobile0s config styles.

--- src/componentes/sorting/style.ts
+++ src/componentes/sorting/style.ts
@@ -12,6 +12,18 @@ export const SortingStyled = styled.div`
     padding-top: 3vw;
     padding-bottom: 1vw;
     height: 2.5vw;
+    //para mobiles
+    @media (max-width: 400px) {
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        width: max-content;
+        margin-left: 0;
+        margin-bottom: 0;
+        padding-top: 0;
+        padding-bottom: 0;
+        height: fit-content;
+    }
 `;
 
 export const TextSelectStyled = styled.p`
@@ -19,6 +31,10 @@ export const TextSelectStyled = styled.p`
     font-weight: 600;
     margin-right: 0.6vw;
     font-family: var(--font-plusJ-variable);
+    //para mobiles
+    @media (max-width: 400px) {
+         display: none;
+    }
 `;
 export const SelectStyled = styled.select`
     font-size: 1vw;
@@ -26,10 +42,25 @@ export const SelectStyled = styled.select`
     padding: 0.5vw;
     font-family: var(--font-plusJ-variable);
     border-radius: 0.5vw;
+    //para mobiles
+    @media (max-width: 400px) {
+        font-size: 3vw;
+        font-weight: 600;
+        padding: 0;
+        font-family: var(--font-plusJ-variable);
+        border-radius: 0.5vw;
+        width: 100%;
+    }
 `;
 
 export const OptionStyled = styled.option`
     padding: 1vw;
     font-weight: 600;
     font-family: var(--font-plusJ-variable);
+    //para mobiles
+    @media (max-width: 400px) {
+         padding: 3vw;
+         font-weight: 600;
+         font-family: var(--font-plusJ-variable);
+    }
 `;
